da7003e5622a4a166be8120b403895387eae8304
Greg Ecock <gecock14@vt.edu>
2012-04-27 18:29:21 -0300

    Error Checking added

--- Process.cpp
+++ Process.cpp
@@ -31,13 +31,27 @@ Process::Process(const std::vector<char*>& args, bool verbose) :
 	throw std::string("Process fork");
     } else if ( m_pid == 0 ) {
 	/* child process */
-
-	close( PARENT_WRITE );
-	close( PARENT_READ );
-	dup2( CHILD_WRITE, 1);
-	close( CHILD_WRITE );
-	dup2( CHILD_READ, 0);
-	close(CHILD_READ);
+	
+	if (close( PARENT_WRITE ) == -1 || close( PARENT_READ ) == -1){
+	    	perror("Parent File Desciptors Faled to Close");
+	    	throw std::string("Parent File Desciptors Faled to Close);
+	}
+	if(dup2( CHILD_WRITE, 1) == -1){
+		perror("Duplicating CHILD_WRITE Failed");
+		throw std::string("Duplicating CHILD_WRITE Failed");		
+	}
+	if(close( CHILD_WRITE ) == -1){
+	    	perror("Child File Desciptors Faled to Close");
+	    	throw std::string("Child File Desciptors Faled to Close");		
+	}
+	if(dup2( CHILD_READ, 0) == -1){
+		perror("Duplicating CHILD_READ Failed");
+		throw std::string("Duplicating CHILD_READ Failed")
+	}
+	if(close(CHILD_READ) == -1){
+	    	perror("Child File Desciptors Faled to Close");
+	    	throw std::string("Child File Desciptors Faled to Close");
+	}
 	execvp(args[0], const_cast<char**>(&args[0]));
 	perror("Process execvp");
 	throw std::string("Process execvp");
@@ -45,10 +59,20 @@ Process::Process(const std::vector<char*>& args, bool verbose) :
 	/* parent process */
 	if (verbose)
         std::cerr << "Process " << m_name << ": forked PID " << m_pid << std::endl;
-	close(CHILD_READ);
-	close(CHILD_WRITE);
+	if(close(CHILD_READ) == -1 || close(CHILD_WRITE) == -1){
+	    	perror("Child File Desciptors Faled to Close");
+	    	throw std::string("Child File Desciptors Faled to Close");		
+	}
 	m_pread = fdopen(PARENT_READ, "r");
+	if(m_pread == NULL){
+	    	perror("Open PARENT_READ Failed");
+	    	throw std::string("Open PARENT_READ Failed");
+	}
 	m_pwrite = fdopen( PARENT_WRITE, "w");
+	if(m_pwrite == NULL){
+	    	perror("Open PARENT_WRITE Failed");
+	    	throw std::string("Open PARENT_WRITE Failed");
+	}
     }
 };
 
@@ -57,11 +81,22 @@ Process::~Process()
     if (verbose)
 	std::cerr << "Process " << m_name << ": Entering ~Process()" << std::endl;
 
-    fclose(m_pwrite);
-    waitpid(m_pid, &rtval, 0);
-    fclose(m_pread);
+    if(fclose(m_pwrite) == EOF){
+    	  perror("Closing Read Descriptor Failed");
+	  throw std::string("Closing Read Descriptor Failed");
+	}
+    kill(m_pid, SIGTERM);
+    int status;
+    pid_t pid = waitpid(m_pid, &status, 0);
+    
+    
     if (pid < 0)
 	perror("~Process waitpid");
+	
+    if(fclose(m_pread) == EOF){
+    	  perror("Open PARENT_READ Failed");
+	  throw std::string("Open PARENT_READ Failed");
+	}
 
     if (verbose)
 	std::cerr << "Process " << m_name << ": Leaving ~Process()" << std::endl;
@@ -69,8 +104,14 @@ Process::~Process()
 
 void Process::write(const std::string& line)
 {
-    fputs(line.c_str(), m_pwrite);
-    fflush(m_pwrite);
+    if(fputs(line.c_str(), m_pwrite) == EOF){
+    	perror("Puts Failed");
+    	throw std::string("Puts Failed");
+    }
+    if(fflush(m_pwrite) == EOF){
+    	perror("Flsuh Failed");
+    	throw std::string("Flush Failed");    	
+    }
 }
 
 std::string Process::read()
@@ -79,7 +120,10 @@ std::string Process::read()
     char* mystring = NULL;
     size_t num_bytes;
 
-    getline(&mystring. &num_bytes, m_pread);
+    if(getline(&mystring. &num_bytes, m_pread) == -1){}
+    	perror("Read Failed");
+    	throw std::string("Read Failed");
+    }
     line = mystring;
     return line;
 }
